c96eb4f39b05ea459973485b2687db30ace360dc
Stefan Kowal <kowalmax.s@gmail.com>
2016-01-14 20:38:28 -0500

    second commit

---
+++ Assets/ArrangeCubes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrangeCubes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		int index = 0;
+		//for each row
+		for (float xVal = 0.0f; xVal < 3.0f; xVal++) {
+			//for each col
+			for (float yVal = 0.0f; yVal < 3.0f; yVal++){
+				string nameVal = "Cube (" + index + ")";
+				//get the child that corresponds to the i
+				//Transform childCube = transform.Find(name);
+				Debug.Log(transform.Find(nameVal).name);
+				//childCube.position = new Vector3(xVal, yVal, 0.0f);
+				index++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
